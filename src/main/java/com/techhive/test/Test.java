9c0572db48ed75572c0f046a4f45ff33ed6a7111
gohilniks <gohilniks@gmail.com>
2019-10-16 16:28:33 +0530

    test

--- src/main/java/com/techhive/test/Test.java
+++ src/main/java/com/techhive/test/Test.java
@@ -3,6 +3,6 @@ package com.techhive.test;
 public class Test {
 	public static void main(String[] args) {
 		System.err.println("hello tester.");
-		System.err.println("niks..");
+		System.err.println("niks...");
 	}
 }
